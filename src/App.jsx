d86aeebd2a994605bbf30b36daa4c2e372758cad
Molar <grubenmj@gmail.com>
2024-06-18 21:22:17 -0600

    todo list ended

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+const [todos, setTodos] = useState([])
+const [text, setText] = useState("")
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function addTodo(){
+  setTodos([...todos,text])
+}
+function onSubmit(event){
+  event.preventDefault();
+  addTodo()
+  setText("")
 }
 
+function removeTodo(indexToRemove){
+  // todos.splice(indexToRemove, 1)
+  // setTodos([todos])
+  const newTodos = todos.filter(((todos, idx) => idx !== indexToRemove))
+  setTodos(newTodos)
+}
+
+  return (
+    <main className="w-full min-h-screen">
+      <form 
+      className="flex flex-row gap-2 justify-center p-5"
+      onSubmit={onSubmit}
+      >
+      <input 
+      type="text" 
+      placeholder="Ingreasa una tarea" 
+      className="p-2 rounded text-black w-full max-w-screen-sm" 
+      value={text}
+      onChange={(event) => setText(event.target.value)}
+      required
+      />
+        <button 
+        className="bg-white text-black px-3 rounded"
+        > 
+        + Agregar
+        </button>
+      </form>
+      <div className="max-w-screen-sm mx-auto p-4 flex flex-col gap-2">
+        {
+          todos.length === 0 && <p className="text-white/50">No tienes tareas pendientes 🤷‍♂️</p>
+        }
+        {
+          todos.map((todo, idx)=> {
+            return <div key={`todo-${idx}`} className="bg-white/10 rounded p-4 flex flex-row justify-between">
+              <span className="select-none">
+              {todo}
+              </span>
+              <span className="text-red-500 cursor-pointer select-none hover:bg-red-500 hover:text-white rounded p-1 text-center " onClick={() => removeTodo(idx)}> 
+              X
+            </span>
+            </div>
+            
+          }) 
+        }
+      </div>
+    </main>
+  )
+}
 export default App
